dc652a825799f224385727577cafd7a5e6ddacab
Lukáš Hanák <lukas.hanak@protonmail.com>
2020-03-01 02:17:04 +0100

    small changes

--- request.php
+++ request.php
@@ -1,7 +1,9 @@
 <?php
 // request.php
-$data['temp'] = round(shell_exec("sudo python temp.py"), 3);
-$data['pressure'] = round(shell_exec("sudo python pressure.py"), 3);
-$data['humidity'] = round(shell_exec("sudo python humidity.py"), 3);
+$data['temp'] = round(shell_exec("sudo python ./senzors/temp.py"), 3);
+$data['pressure'] = round(shell_exec("sudo python ./sensors/pressure.py"), 3);
+$data['humidity'] = round(shell_exec("sudo python ./sensors/humidity.py"), 3);
+// $data['humidity'] = round(shell_exec("sudo python ./sensors/humidity.py"), 3);
+// $data['humidity'] = round(shell_exec("sudo python ./sensors/humidity.py"), 3);
 echo json_encode($data);
 ?>
