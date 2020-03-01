c6611ad90241504992bea8eed1b3d1882da21894
Lukáš Hanák <lukas.hanak@protonmail.com>
2020-03-01 02:18:38 +0100

    moved js from index.html to main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+setInterval(
+    function(){
+      GetData();
+    }
+  , 3000);
 
+function GetData(){
+  //console.log("Aktualizovano");
+  $.ajax(
+  'request.php',
+ {
+      success: function(data) {
+        var DATA = jQuery.parseJSON(data) //json_decode(data, true );
+       $('#temp').html((DATA.temp).toFixed(2) + "  &#8451;");
+       $('#pressure').html(DATA.pressure + " hPa");
+        $('#humidity').html(DATA.humidity + " %");
+      },
+      error: function() {
+        alert('God Damn Error');
+     }
+   }
+  );
+}
