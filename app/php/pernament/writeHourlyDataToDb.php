fed4fb31d76b66c777a9759cbb2bc706bfe4b937
Lukas Hanak <lukas.hanak@protonmail.com>
2020-09-21 17:20:56 -0400

    added using php variables inside mysql query

--- app/php/pernament/writeHourlyDataToDb.php
+++ app/php/pernament/writeHourlyDataToDb.php
@@ -1,15 +1,16 @@
 <?php
 require 'connectPernamentDb.php';
 
-$sql = "SELECT * FROM temperature_today WHERE temp_time BETWEEN '12:00:00' AND '13:00:00'";
-$result = $conn->query($sql);
+$startTime = date("H");
+$endTime = date("H")-1;
+
+$result = mysqli_query($conn,"SELECT `temp_value` FROM `temp` WHERE temp_time BETWEEN '".$startTime."' AND '".$endTime."'");
 
 $arrayOfValues = array(); // array which will contain all returned values
 $index = 0;
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
       $arrayOfValues[$index] = $row["temp_value"];
-      // echo "\r\n";
       $index+=1;
     }
   }  else {
@@ -17,13 +18,16 @@ if ($result->num_rows > 0) {
   }
   $conn->close();
 
+
   // get average value and return it
   $delka = sizeof($arrayOfValues);
   $soucet = 0;
   for ($i=0; $i<$delka; $i++) {
       $soucet+=$arrayOfValues[$i];
+      echo $arrayOfValues[$i];
+      echo "\n";
   }
-  echo round(($soucet / $delka),2);
+  // echo round(($soucet / $delka),2);
 
 ?>
 
