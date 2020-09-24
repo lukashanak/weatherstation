bf8a912eee7a2d96ff8a8a545b1ae66b7d94a3fd
Lukas Hanak <lukas.hanak@protonmail.com>
2020-09-24 12:31:34 +0200

    finished the script :))

--- app/php/planedTasks/writeHourlyAvgToDb.php
+++ app/php/planedTasks/writeHourlyAvgToDb.php
@@ -1,15 +1,12 @@
 <?php
 
+// this script will always at a time like this (23:00:00, 0:00:00 etc.)
+
 function getHourlyAvg() {
 require '../config/connectTodayDb.php';
 
 $startTime = (date("H")-1) . ":00:00";
 $endTime = date("H") . ":00:00";
-
-/*
-$startTime = (date("H")) . ":00:00";
-$endTime = (date("H")+1 ). ":00:00";
-*/
 
 $result = mysqli_query($conn,"SELECT AVG(temp_value) AS 'AverageTemp' FROM `temp` WHERE temp_time BETWEEN '".$startTime."' AND '".$endTime."'");
 
@@ -24,12 +21,21 @@ if ($result->num_rows > 0) {
 
 function writeToDb() {
 require '../config/connectPernamentDb.php';
-$temp = round(getHourlyAvg(),2);
-$date = date('Y-m-d');
-$time = date("H:00:00"); 
 
-$stmt = $conn->prepare("INSERT INTO temperature (temp_date, temp_time, temp_value) VALUES (?, ?, ?)");
-$stmt->bind_param("ssd", $date, $time, $temp);
+$temp_avg_value = round(getHourlyAvg(),2);
+
+$start_time = (date("H")-1) . ":00:00";
+$end_time = date("H") . ":00:00";
+$date = date('Y-m-d');
+
+if ($start_time == '23:00:00') {
+  $date = date_create(date("Y-m-d"));
+  date_sub($date, date_interval_create_from_date_string('1 days'));
+  $date = date_format($date, 'Y-m-d');
+}
+
+$stmt = $conn->prepare("INSERT INTO temp (temp_date, temp_start_time, temp_end_time, temp_avg_value) VALUES (?, ?, ?, ?)");
+$stmt->bind_param("sssd", $date, $start_time, $end_time, $temp_avg_value);
 $stmt->execute();
 echo "Temp wrote successfully \n";
 }
