de9611fdfd0ba87a90f212e21d7fa9cf4cd550c8
Lukas Hanak <lukas.hanak@protonmail.com>
2020-09-29 19:49:53 +0200

    switched from avg values to normal values (which makes much more sense)

--- app/php/planedTasks/writeHourlyAvgToDb.php
+++ app/php/planedTasks/writeHourlyAvgToDb.php
@@ -5,8 +5,9 @@
 function getHourlyAvg($sensor) {
 require '../config/connectTodayDb.php';
 
-$startTime = (date("H")-1) . ":00:00";
-$endTime = date("H") . ":00:00";
+$startTime = (date("H")) . ":00:00";
+$endTime = (date("H")) . ":05:00";
+
 
 switch ($sensor) {
   case 'temp':
@@ -39,38 +40,37 @@ $pressure_avg_value = round(getHourlyAvg('pressure'),2);
 $light_avg_value = round(getHourlyAvg('light'),2);
 // $isRaining_avg_value is missing because it doesn't yet
 
-$start_time = (date("H")-1) . ":00:00";
-$end_time = date("H") . ":00:00";
+$time = (date("H")) . ":00:00";
 $date = date('Y-m-d');
 
-if ($start_time == '23:00:00') {
+if ($time == '23:00:00') {
   $date = date_create(date("Y-m-d"));
   date_sub($date, date_interval_create_from_date_string('1 days'));
   $date = date_format($date, 'Y-m-d');
 }
 
 // temperature
-$stmt = $conn->prepare("INSERT INTO temp (date, start_time, end_time, avg_value) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("sssd", $date, $start_time, $end_time, $temp_avg_value);
+$stmt = $conn->prepare("INSERT INTO temp (date, time, value) VALUES (?, ?, ?)");
+$stmt->bind_param("ssd", $date, $time, $temp_avg_value);
 $stmt->execute();
 echo "Temp wrote successfully \n";
 
 
 // humidity
-$stmt = $conn->prepare("INSERT INTO humidity (date, start_time, end_time, avg_value) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("sssd", $date, $start_time, $end_time, $humidity_avg_value);
+$stmt = $conn->prepare("INSERT INTO humidity (date, time, value) VALUES (?, ?, ?)");
+$stmt->bind_param("ssd", $date, $time, $humidity_avg_value);
 $stmt->execute();
 echo "humidity wrote successfully \n";
 
 // pressure
-$stmt = $conn->prepare("INSERT INTO pressure (date, start_time, end_time, avg_value) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("sssd", $date, $start_time, $end_time, $pressure_avg_value);
+$stmt = $conn->prepare("INSERT INTO pressure (date, time, value) VALUES (?, ?, ?)");
+$stmt->bind_param("ssd", $date, $time, $pressure_avg_value);
 $stmt->execute();
 echo "pressure wrote successfully \n";
 
 // light
-$stmt = $conn->prepare("INSERT INTO light (date, start_time, end_time, avg_value) VALUES (?, ?, ?, ?)");
-$stmt->bind_param("sssd", $date, $start_time, $end_time, $light_avg_value);
+$stmt = $conn->prepare("INSERT INTO light (date, time, value) VALUES (?, ?, ?)");
+$stmt->bind_param("ssd", $date, $time, $light_avg_value);
 $stmt->execute();
 echo "light wrote successfully \n";
 
