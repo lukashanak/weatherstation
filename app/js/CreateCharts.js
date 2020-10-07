9ba8e8ba0f5b72140f3a2d10007f7c3d40b43b51
Lukas Hanak <lukas.hanak@protonmail.com>
2020-10-07 20:46:22 +0200

    refactoring

--- app/js/CreateCharts.js
+++ app/js/CreateCharts.js
@@ -1,5 +1,20 @@
+// DOM ELEMENTS
 const dailyChartTemp = document.getElementById("dailyChartTemp");
 
+
+let config = {
+  sensors: ['temp', 'humidity', 'pressure', 'light'],
+  ranges: ['today', 'yesterday', 'lastWeek', 'lastMonth'],
+  todayDOM: {
+    temp: document.getElementById("dailyChartTemp"),
+  },
+  yesterdayDOM: {
+
+  }
+}
+
+
+// DEFAULT APEXCHART CONFIG
 const defaultOptions = {
   colors:['#F44336'],
   title: {
@@ -32,59 +47,48 @@ const defaultOptions = {
 }
 
 
+// RUN FUNCTION TODAY() ON LOAD WHICH CREATES DAILY CHART FOR EVERY SENSOR
+window.onload = function() {
+  today();
+}
+
+
+// function which does it all for today charts
+function today(range, sensor) {
+  $.get("./php/getJsonFromDb/today/today.php", function(data, status){
+   let convertedData = convertData(JSON.parse(data));
+   createGraph(convertedData.values, convertedData.categories);
+    }
+    )}
+
+
+
+
+    
+// ------->  SUB-FUNCTIONS (used for today())
+
+
+function convertData(data) {
+let result = {
+  values: [],
+  categories: []
+}
+for (let i=0; i < data.length; i++) {
+result.values[i] = data[i].split(",")[0];
+result.categories[i] = data[i].split(",")[2];
+}
+return result;
+}
+
 var createGraph = (sensorValues, categories) => {
   let options = Object.assign({}, defaultOptions);
   options.xaxis.categories=categories;
   let rendered = new ApexCharts(document.querySelector("#dailyChartTemp"), options);
   rendered.render();
-  console.log(sensorValues);
   rendered.appendData([{
     data: sensorValues
   }]);
 }
 
-
-window.onload = function() {
-  today();
-}
-
-// function which does it all for today charts
-function today(range, sensor) {
-    $.get("./php/getJsonFromDb/today/today.php", function(data, status){
-     let convertedData = convertData(JSON.parse(data));
-     createGraph(convertedData.values, convertedData.categories);
-      }
-      )}
-
-function convertData(data) {
-  let result = {
-    values: [],
-    categories: []
-  }
-for (let i=0; i < data.length; i++) {
-  result.values[i] = data[i].split(",")[0];
-  result.categories[i] = data[i].split(",")[2];
-}
-return result;
-}
-
-
-
-let config = {
-  sensors: ['temp', 'humidity', 'pressure', 'light'],
-  ranges: ['today', 'yesterday', 'lastWeek', 'lastMonth'],
-  todayDOM: {
-    temp: document.getElementById("dailyChartTemp"),
-  },
-  yesterdayDOM: {
-
-  }
-}
-
-
-/*
-    tempYesterday: document.getElementById("yesterdayChartTemp"),
-    tempLastWeek: document.getElementById("tempLastWeek")
-*/
 
 
