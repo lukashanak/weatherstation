0f3f0f0d8f419141c0f4660c6cabdd17f6129f2f
Lukas Hanak <lukas.hanak@protonmail.com>
2020-10-08 21:28:56 +0200

    added all sensors

--- app/js/modules/UpdateValues.js
+++ app/js/modules/UpdateValues.js
@@ -1,20 +1,26 @@
 
 
 //onload
-updateTemp();
+updateActualValue("temp");
+updateActualValue("pressure");
+updateActualValue("humidity");
+updateActualValue("light");
 
 // update values like temp, pressure.. every 5 seconds
 setInterval(
   function(){
-  updateTemp();
+    updateActualValue("temp");
+    updateActualValue("pressure");
+    updateActualValue("humidity");
+    updateActualValue("light");
   }
 , 5000);
 
-function updateTemp(){
+function updateActualValue(sensor){
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      DOMConfig_UpdateValues.temp.innerHTML=this.responseText;
+      DOMConfig_UpdateValues[sensor].innerHTML=this.responseText;
     }
   };
   xhttp.open("GET", "./php/getLastValue/temp.php", true);
