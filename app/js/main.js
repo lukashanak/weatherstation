a3619b56144b003f32a03575358850f93c3ea33e
Lukas Hanak <lukas.hanak@protonmail.com>
2020-12-06 09:36:10 +0100

    created main.js

---
+++ app/js/main.js
@@ -0,0 +1,6 @@
+import {renderChart} from './modules/CreateCharts.js'
+
+renderChart("yesterday", "temp");
+renderChart("yesterday", "pressure");
+renderChart("yesterday","humidity");
+renderChart("yesterday","light");
