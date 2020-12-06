ffc7f8bc7c6c1ec1eac891dbf3f1a3bd8dfc95b0
Lukas Hanak <lukas.hanak@protonmail.com>
2020-12-06 09:35:07 +0100

    new files

---
+++ app/php/getJsonFromDb/lastMonth/pressure.php
@@ -0,0 +1,21 @@
+<?php
+require '../../config/connectPernamentDb.php';
+
+$startDate = date('Y-m-d', strtotime("-29 days"));
+$endDate = date('Y-m-d');
+$result = mysqli_query($conn,"SELECT value, time, date FROM `pressure` WHERE date BETWEEN '".$startDate."' AND '".$endDate."' ");
+
+$tempToday = array();
+
+if ($result->num_rows > 0) {
+  $i = 0;
+    while($row = $result->fetch_assoc()) {
+      $tempToday[$i] = ($row["value"] . ", " . $row["date"] . ", " . $row["time"]);
+      $i++;
+    }
+  }
+echo json_encode($tempToday);
+
+$conn->close();
+
+?>
--- app/php/getJsonFromDb/yesterday/temp.php
+++ app/php/getJsonFromDb/yesterday/temp.php
@@ -1,12 +1,7 @@
 <?php
 require '../../config/connectPernamentDb.php';
 
-/*
-$date = date('Y-m-d');
-$result = mysqli_query($conn,"SELECT value, time, date FROM `temp` WHERE date = '".$date."' ");
-*/
-
-$date = date('Y-m-d');
+$date = date('Y-m-d', strtotime("-1 days"));
 $result = mysqli_query($conn,"SELECT value, time, date FROM `temp` WHERE date = '".$date."' ");
 
 $tempToday = array();
@@ -14,9 +9,7 @@ $tempToday = array();
 if ($result->num_rows > 0) {
   $i = 0;
     while($row = $result->fetch_assoc()) {
-      $x = explode(":", $row["time"]);
-      $time = $x[0] . ":"  . $x[1];
-      $tempToday[$i] = ($row["value"] . ", " . $row["date"] . ", " . $time);
+      $tempToday[$i] = ($row["value"] . ", " . $row["date"] . ", " . $row["time"]);
       $i++;
     }
   }
